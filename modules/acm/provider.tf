bad8a01eb0d2f44f6883c0abfb0a068432a3f01c
Jose Ernesto Suarez <ernesto@wedoops.io>
2022-12-07 16:58:43 +0100

    Remove governance account

--- modules/acm/provider.tf
+++ modules/acm/provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "us-east-1"
-  assume_role {
-    role_arn    = local.assume_role_arn
-  }
+ # assume_role {
+ #   role_arn    = local.assume_role_arn
+ # }
   default_tags {
     tags = {
       environment = var.environment
--- modules/dns/provider.tf
+++ modules/dns/provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = var.region
-  assume_role {
-    role_arn    = local.assume_role_arn
-  }
+# assume_role {
+#   role_arn    = local.assume_role_arn
+# }
   default_tags {
     tags = {
       Env = var.environment
